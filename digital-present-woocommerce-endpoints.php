cb04edab4ca8e32e2bd247372213d5832f9f9383
hnikoloski <h.nikoloski@outlook.com>
2022-04-04 13:48:03 +0200

    v test

--- digital-present-woocommerce-endpoints.php
+++ digital-present-woocommerce-endpoints.php
@@ -5,13 +5,13 @@
  *
  * @package       DIGIPWC
  * @author        Digital Present
- * @version       1.0.0
+ * @version       1.0.1
  *
  * @wordpress-plugin
  * Plugin Name:   Woocommerce Endpoints
  * Plugin URI:    https://github.com/hnikoloski/wc-endpoints.git
  * Description:   Because Woocommerce markup sucks here are some endpoints
- * Version:       1.0.0
+ * Version:       1.0.1
  * Author:        Digital Present
  * Author URI:    https://digitalpresent.io/
  * Text Domain:   digital-present-woocommerce-endpoints
